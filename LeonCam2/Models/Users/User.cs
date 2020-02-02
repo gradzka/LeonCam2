f89d62b0c3255caf9cd2ce4414df9ca01844b697
gradzka <monika.gradzka@tlen.pl>
2020-02-02 18:20:59 +0100

    Add User and UserMap

---
+++ LeonCam2/Models/Users/User.cs
@@ -0,0 +1,29 @@
+﻿// User.cs by Gradzka & Kazimierczak
+
+namespace LeonCam2.Models
+{
+    using System;
+
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string SecurityQuestions { get; set; }
+
+        public string Answer { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime LastLoginAttemptDate { get; set; }
+
+        public DateTime LastLogoutDate { get; set; }
+
+        public int LoginAttemptCounter { get; set; }
+
+        public int RedAlertDeleteSettingID { get; set; }
+
+        public int GreenAlertDeleteSettingID { get; set; }
+    }
+}
