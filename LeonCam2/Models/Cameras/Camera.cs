b826d9a3ecb218a8cd2f9e4dbef0acb09046a3cf
gradzka <monika.gradzka@tlen.pl>
2020-02-02 18:20:27 +0100

    Add Camera and CameraMap

---
+++ LeonCam2/Models/Cameras/Camera.cs
@@ -0,0 +1,17 @@
+﻿// Camera.cs by Gradzka & Kazimierczak
+
+namespace LeonCam2.Models
+{
+    public class Camera : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string IPAddress { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
