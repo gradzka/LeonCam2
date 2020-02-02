9a8f8e2308be8a40dd98835bb790db74d2e89a2f
gradzka <monika.gradzka@tlen.pl>
2020-02-02 18:21:52 +0100

    Add BaseEntity

---
+++ LeonCam2/Models/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿// BaseEntity.cs by Gradzka & Kazimierczak
+
+namespace LeonCam2.Models
+{
+    using System;
+
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
