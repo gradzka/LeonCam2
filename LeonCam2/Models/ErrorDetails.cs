510e29f0eda51d1e19d93f28c5b90dd6713c81d3
gradzka <monika.gradzka@tlen.pl>
2020-02-04 21:58:30 +0100

    Add ErrorDetails

---
+++ LeonCam2/Models/ErrorDetails.cs
@@ -0,0 +1,18 @@
+﻿// ErrorDetails.cs by Gradzka & Kazimierczak
+
+namespace LeonCam2.Models
+{
+    using Newtonsoft.Json;
+
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
