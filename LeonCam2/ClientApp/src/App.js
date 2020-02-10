a43eb44b93a7507993b78ffdbee61ede4a4e7096
gradzka <monika.gradzka@tlen.pl>
2020-02-10 20:34:20 +0100

    Import Home component

--- LeonCam2/ClientApp/src/App.js
+++ LeonCam2/ClientApp/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import { Home } from './components/Home/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
