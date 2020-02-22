694cbd5c4910798f6e13de03291202370d99a9e3
gradzka <monika.gradzka@tlen.pl>
2020-02-23 00:39:32 +0100

    Add footer

---
+++ LeonCam2/ClientApp/src/components/Shared/Footer.js
@@ -0,0 +1,43 @@
+﻿import React, { Component } from 'react';
+import './Footer.css';
+
+export class Footer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <footer>
+                    <ul>
+                        <li><b>LeonCam2</b> is the surveillance system with IP network cameras. It supports the ONVIF standard. </li>
+                     </ul>                
+                    <ul>
+                        <li>
+                            <span> See source code </span>
+                            <a href="https://github.com/gradzka/LeonCam2">
+                                <i class="fa fa-github"/>
+                            </a>
+                        </li>
+                        <li className="divider"/>
+                        <li>
+                            <span> 2020 </span> 
+                            <a href="https://github.com/gradzka">Gradzka</a>
+                            <span> & </span> 
+                            <a href="https://github.com/kazimierczak-robert">Kazimierczak</a>
+                        </li>
+                        <li className="divider" />
+                        <li> 
+                            <span> Design by Kuzayova</span>
+                            <a href="https://www.facebook.com/kuzayova">
+                                <i class="fa fa-facebook-f"/>
+                            </a>
+                            <a href="https://www.instagram.com/kuzayova.art">
+                                <i class="fa fa-instagram"/>
+                            </a>
+                        </li>
+                    </ul>
+            </footer>
+        );
+    }
+}
--- LeonCam2/ClientApp/src/index.js
+++ LeonCam2/ClientApp/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'font-awesome/css/font-awesome.min.css';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
