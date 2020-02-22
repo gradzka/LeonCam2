e26aac686d7e959b1997445d60e62b4cd9447fb4
gradzka <monika.gradzka@tlen.pl>
2020-02-23 00:36:51 +0100

    Remove first card

--- LeonCam2/ClientApp/src/components/Home/Home.js
+++ LeonCam2/ClientApp/src/components/Home/Home.js
@@ -44,82 +44,68 @@ export class Home extends Component {
 
     render() {
         return (
-            <div className="container-fluid content-row row">
-                <div className="col-sm-4" style={{ margin: "0 auto 20px" }}>
-                    <div className="card" style={{ height: "100%" }}>
-                        <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card-container col-sm-8">
-                        <div className="card alt hidden">
-                            <div className="toggle" tabIndex="0" onClick={this.openRegisterButton}>SIGN UP</div>
-                            <h1 className="title">Register
+            <div className="card-container">
+                <div className="card alt hidden">
+                    <div className="toggle" tabIndex="0" onClick={this.openRegisterButton}>SIGN UP</div>
+                    <h1 className="title">Register
                                 <div className="close" tabIndex="0" onClick={this.closeRegisterButton}></div>
-                            </h1>
-                            <form>
-                                <div className="input-container">
-                                    <input type="text" id="regname" required="required" />
-                                    <label forhtml="regname">Username</label>
-                                    <div className="bar"></div>
-                                </div>
-                                <div className="input-container">
-                                    <input type="password" id="regpass" required="required" />
-                                    <label forhtml="regpass">Password</label>
-                                    <div className="bar"></div>
-                                </div>
-                                <div className="input-container">
-                                    <input type="password" id="reregpass" required="required" />
-                                    <label forhtml="reregpass">Repeat Password</label>
-                                    <div className="bar"></div>
-                                </div>
-                                <div className="button-container">
-                                    <button><span>sign up</span></button>
-                                </div>
-                            </form>
+                    </h1>
+                    <form>
+                        <div className="input-container">
+                            <input type="text" id="regname" required="required" />
+                            <label forhtml="regname">Username</label>
+                            <div className="bar"></div>
                         </div>
-                        <div className="card alt hidden bottom">
-                            <div className="toggle" tabIndex="0" onClick={this.openForgotPasswordButton}>Forgot password?</div>
-                            <h1 className="title">Forgot<br/>password?
+                        <div className="input-container">
+                            <input type="password" id="regpass" required="required" />
+                            <label forhtml="regpass">Password</label>
+                            <div className="bar"></div>
+                        </div>
+                        <div className="input-container">
+                            <input type="password" id="reregpass" required="required" />
+                            <label forhtml="reregpass">Repeat Password</label>
+                            <div className="bar"></div>
+                        </div>
+                        <div className="button-container">
+                            <button><span>sign up</span></button>
+                        </div>
+                    </form>
+                </div>
+                <div className="card alt hidden bottom">
+                    <div className="toggle" tabIndex="0" onClick={this.openForgotPasswordButton}>Forgot password?</div>
+                    <h1 className="title">Forgot<br />password?
                                 <div className="close" tabIndex="0" onClick={this.closeForgotPasswordButton}></div>
-                            </h1>
-                            <form>
-                                <div className="input-container">
-                                    <input type="text" id="forgotmail" required="required" />
-                                    <label forhtml="regname">E-Mail</label>
-                                    <div className="bar"></div>
-                                </div>
-                                <div className="button-container">
-                                    <button><span>Send link</span></button>
-                                </div>
-                            </form>
+                    </h1>
+                    <form>
+                        <div className="input-container">
+                            <input type="text" id="forgotmail" required="required" />
+                            <label forhtml="regname">E-Mail</label>
+                            <div className="bar"></div>
                         </div>
-                        <div className="card first"></div>
-                        <div className="card">
-                            <h1 className="title">Login</h1>
-                            <form>
-                                <div className="input-container">
-                                    <input type="text" id="name" required="required" />
-                                    <label forhtml="name">Username</label>
-                                    <div className="bar"></div>
-                                </div>
-                                <div className="input-container">
-                                    <input type="password" id="pass" required="required" />
-                                    <label forhtml="pass">Password</label>
-                                    <div className="bar"></div>
-                                </div>
-                                <div className="button-container">
-                                    <button><span>Sign in</span></button>
-                                </div>
-                                <div className="footer"></div>
-                            </form>
+                        <div className="button-container">
+                            <button><span>Send link</span></button>
                         </div>
-                    </div>
+                    </form>
+                </div>
+                <div className="card first"></div>
+                <div className="card">
+                    <h1 className="title">Login</h1>
+                    <form>
+                        <div className="input-container">
+                            <input type="text" id="name" required="required" />
+                            <label forhtml="name">Username</label>
+                            <div className="bar"></div>
+                        </div>
+                        <div className="input-container">
+                            <input type="password" id="pass" required="required" />
+                            <label forhtml="pass">Password</label>
+                            <div className="bar"></div>
+                        </div>
+                        <div className="button-container">
+                            <button><span>Sign in</span></button>
+                        </div>
+                        <div className="footer"></div>
+                    </form>
                 </div>
             </div>
         );
