d37900eb79e02504d0fd4c91b861d30d4c624a2a
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2020-02-23 01:20:30 +0100

    Center NavbarBrand, getCurrentYear in footer

--- LeonCam2/ClientApp/src/components/NavMenu.js
+++ LeonCam2/ClientApp/src/components/NavMenu.js
@@ -25,8 +25,8 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">LeonCam2</NavbarBrand>
+                <Container>
+                    <NavbarBrand tag={Link} to="/"><b>LeonCam2</b></NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
--- LeonCam2/ClientApp/src/components/Shared/Footer.js
+++ LeonCam2/ClientApp/src/components/Shared/Footer.js
@@ -6,37 +6,41 @@ export class Footer extends Component {
         super(props);
     }
 
+    getCurrentYear() {
+        return new Date().getFullYear();
+    }
+
     render() {
         return (
             <footer>
-                    <ul>
-                        <li><b>LeonCam2</b> is the surveillance system with IP network cameras. It supports the ONVIF standard. </li>
-                     </ul>                
-                    <ul>
-                        <li>
-                            <span> See source code </span>
-                            <a href="https://github.com/gradzka/LeonCam2">
-                                <i class="fa fa-github"/>
-                            </a>
-                        </li>
-                        <li className="divider"/>
-                        <li>
-                            <span> 2020 </span> 
-                            <a href="https://github.com/gradzka">Gradzka</a>
-                            <span> & </span> 
-                            <a href="https://github.com/kazimierczak-robert">Kazimierczak</a>
-                        </li>
-                        <li className="divider" />
-                        <li> 
-                            <span> Design by Kuzayova</span>
-                            <a href="https://www.facebook.com/kuzayova">
-                                <i class="fa fa-facebook-f"/>
-                            </a>
-                            <a href="https://www.instagram.com/kuzayova.art">
-                                <i class="fa fa-instagram"/>
-                            </a>
-                        </li>
-                    </ul>
+                <ul>
+                    <li><b>LeonCam2</b> is the surveillance system with IP network cameras. It supports the ONVIF standard. </li>
+                </ul>
+                <ul>
+                    <li>
+                        <span>See source code</span>
+                        <a href="https://github.com/gradzka/LeonCam2">
+                            <i class="fa fa-github" />
+                        </a>
+                    </li>
+                    <li className="divider" />
+                    <li>
+                        <span>{this.getCurrentYear()}</span>
+                        <a href="https://github.com/gradzka">Gradzka</a>
+                        <span> & </span>
+                        <a href="https://github.com/kazimierczak-robert">Kazimierczak</a>
+                    </li>
+                    <li className="divider" />
+                    <li>
+                        <span>Design by Kuzayova</span>
+                        <a href="https://www.facebook.com/kuzayova">
+                            <i class="fa fa-facebook-f" />
+                        </a>
+                        <a href="https://www.instagram.com/kuzayova.art">
+                            <i class="fa fa-instagram" />
+                        </a>
+                    </li>
+                </ul>
             </footer>
         );
     }
