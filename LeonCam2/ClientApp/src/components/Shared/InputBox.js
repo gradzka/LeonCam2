6e142ee6b813fa76b67923d4b28b4e13402780ed
gradzka <monika.gradzka@tlen.pl>
2020-02-24 21:55:47 +0100

    InputBox with capsLk and hidePassword

--- LeonCam2/ClientApp/src/components/Shared/InputBox.js
+++ LeonCam2/ClientApp/src/components/Shared/InputBox.js
@@ -1,10 +1,17 @@
 ﻿import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './InputBox.css';
 
 export class InputBox extends Component {
 
     constructor(props) {
-        super(props);
+        super(props);  
+    }
+
+    hidePassword(event) {
+        event.target.classList.remove("fa-eye");
+        event.target.classList.add("fa-eye-slash");
+        event.target.parentNode.parentNode.childNodes[0].type = 'password';
     }
 
     showPassword(event) {
@@ -13,22 +20,33 @@ export class InputBox extends Component {
         event.target.parentNode.parentNode.childNodes[0].type = 'text'; 
     }
 
-    hidePassword(event) {
-        event.target.classList.remove("fa-eye");
-        event.target.classList.add("fa-eye-slash");
-        event.target.parentNode.parentNode.childNodes[0].type = 'password'; 
+    toggleCapsLkWarning(event) {
+        let inputContainer = event.target.parentNode;
+
+        if (event.getModifierState('CapsLock')) {
+            inputContainer.childNodes[1].childNodes[2].style.visibility = "visible";
+        }
+        else {
+            inputContainer.childNodes[1].childNodes[2].style.visibility = "hidden";
+        }
+    }
+
+    hideCapsLkWarning(event) {
+        let inputContainer = event.target.parentNode;
+        inputContainer.childNodes[1].childNodes[2].style.visibility = "hidden";
     }
 
     render() {
         const isInputPasswordType = this.props.type === "password";
         let eye = isInputPasswordType ? <div className="input-group-addon">
-            <span className="fa fa-eye-slash" aria-hidden="true" onMouseDown={this.showPassword} onMouseUp={this.hidePassword} />
+            <span className="fa fa-eye-slash" aria-hidden="true" onMouseDown={this.showPassword} onMouseUp={this.hidePassword}/>
         </div> : null;
+        let capsLk = isInputPasswordType ? <span id="capsLk" className="badge badge-warning">CapsLk</span> : null;
 
         return (
             <div className={"input-container " + (isInputPasswordType ? 'password-container ' : '') + (this.props.className === undefined ? '' : this.props.className)}>
-                <input type={this.props.type} id={this.props.id} required="required" />
-                <label forhtml={this.props.id}>{this.props.placeholder}</label>
+                <input type={this.props.type} id={this.props.id} required="required" onKeyDown={isInputPasswordType ? this.toggleCapsLkWarning : null} onBlur={isInputPasswordType ? this.hideCapsLkWarning : null} />
+                <label forhtml={this.props.id}>{this.props.placeholder} {capsLk} </label>
                 {eye}
                 <div className="bar"></div>
             </div>
