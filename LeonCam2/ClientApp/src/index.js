2f32aba04b334743cf35523d370caefc0a29f5fa
gradzka <monika.gradzka@tlen.pl>
2020-05-13 21:56:11 +0200

    Fix index.js router

--- LeonCam2/ClientApp/src/index.js
+++ LeonCam2/ClientApp/src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
+import { Router } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.min.css';
