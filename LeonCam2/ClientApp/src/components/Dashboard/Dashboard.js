6eb111a1df85b0872bdcca28ad3c1f5302fd0da3
gradzka <monika.gradzka@tlen.pl>
2020-07-21 23:28:09 +0200

    Add camera containers to dashboard, add array-move and react-sortable-hoc

--- LeonCam2/ClientApp/src/components/Dashboard/Dashboard.js
+++ LeonCam2/ClientApp/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,69 @@
 ﻿import React, { Component } from 'react';
+import { SortableContainer, SortableElement } from "react-sortable-hoc";
+import arrayMove from "array-move";
+import { Container, Row, Col } from 'reactstrap';
+import "./Dashboard.css";
+
+const SortableItem = SortableElement(({ value }) => {
+    return (
+        <div className="dashboard-grid-cell">
+            <Container>
+                <Row>
+                    {value}
+                </Row>
+                <Row>
+                    <Col><div className="circle"><i className="fa fa-power-off" /></div></Col>
+                    <Col><div className="circle"><i className="fa fa-camera" /></div></Col>
+                    <Col><div className="circle"><i className="fa fa-edit" /></div></Col>
+                    <Col><div className="circle"><i className="fa fa-trash" /></div></Col>
+                </Row>
+            </Container>
+        </div>
+    );
+});
+
+const SortableList = SortableContainer(({ items }) => {
+    return (
+        <div>
+            {items.map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value} />
+            ))}
+        </div>
+    );
+});
+
+class SortableComponent extends Component {
+    state = {
+        items: [
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 4",
+            "Item 5",
+            "Item 6",
+        ]
+    };
+
+    componentDidMount() { }
+
+    onSortEnd = ({ oldIndex, newIndex }) => {
+        this.setState(({ items }) => ({
+            items: arrayMove(items, oldIndex, newIndex)
+        }));
+    };
+    render() {
+        return (
+            <div className="dashboard-grid">
+                <SortableList axis="xy" items={this.state.items} onSortEnd={this.onSortEnd}/>
+            </div>
+        );
+    }
+}
 
 export class Dashboard extends Component {
     render() {
         return (
-            <h1 className="title">Logged!</h1>
+            <SortableComponent/>
         );
     }
 }
