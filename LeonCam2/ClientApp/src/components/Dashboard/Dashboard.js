b28fe3e1f6113001752b967c554130aa6290904e
gradzka <monika.gradzka@tlen.pl>
2020-07-26 22:14:54 +0200

    Fix click button

--- LeonCam2/ClientApp/src/components/Dashboard/Dashboard.js
+++ LeonCam2/ClientApp/src/components/Dashboard/Dashboard.js
@@ -4,34 +4,58 @@ import arrayMove from "array-move";
 import { Container, Row, Col } from 'reactstrap';
 import "./Dashboard.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPowerOff, faCamera, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPowerOff, faCamera, faEdit, faTrash, faExpand, faExchangeAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
 
-const CircleActionButton = ({ icon }) => (
-    <Col xs="auto" className="padding-right-left-5">
-        <div className="circle">
-            <FontAwesomeIcon icon={icon}/>
-        </div>
+const CircleActionButton = ({ icon, className, onClickAction }) => (
+    <Col xs="auto" className="padding-right-left-5" >
+        <button className={"circle " + className} onClick={onClickAction}>
+            <FontAwesomeIcon icon={icon} />
+        </button>
     </Col>
 );
 
+class SortableCamera extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOn: false
+        }
+
+        this.powerOnOff = this.powerOnOff.bind(this);
+    }
+
+    powerOnOff(event) {
+        this.setState({
+            isOn: !this.state.isOn
+        });
+    }
+
+    render() {
+        return (
+            <div className="dashboard-grid-cell">
+                <Container>
+                    <Row className="justify-content-center padding-top-bottom-5">
+                        {this.props.value}
+                    </Row>
+                    <Row>
+                        <div className="camera-container"></div>
+                    </Row>
+                    <Row className="justify-content-center padding-top-bottom-5">
+                        <CircleActionButton id="powerOnOff" icon={faPowerOff} onClickAction={this.powerOnOff} className={this.state.isOn ? 'bg-success' : 'bg-danger'} />
+                        <CircleActionButton icon={faExpandAlt} />
+                        <CircleActionButton icon={faEdit} />
+                        <CircleActionButton icon={faCamera} />
+                        <CircleActionButton icon={faTrash} />
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
 const SortableItem = SortableElement(({ value, camera }) => {
     return (
-        <div className="dashboard-grid-cell">
-            <Container>
-                <Row className="justify-content-center padding-top-bottom-5">
-                    {value}
-                </Row>
-                <Row>
-                    <div className="camera-container"></div>
-                </Row>
-                <Row className="justify-content-center padding-top-bottom-5">
-                    <CircleActionButton icon={faPowerOff} />
-                    <CircleActionButton icon={faCamera} />
-                    <CircleActionButton icon={faEdit} />
-                    <CircleActionButton icon={faTrash} />
-                </Row>
-            </Container>
-        </div>
+        <SortableCamera value={value}/>
     );
 });
 
@@ -39,7 +63,7 @@ const SortableList = SortableContainer(({ items }) => {
     return (
         <div>
             {items.map((value, index) => (
-                <SortableItem key={`item-${index}`} index={index} value={value} />
+                <SortableItem key={`item-${index}`} index={index} value={value}/>
             ))}
         </div>
     );
@@ -63,10 +87,15 @@ class SortableComponent extends Component {
             items: arrayMove(items, oldIndex, newIndex)
         }));
     };
+
     render() {
         return (
             <div className="dashboard-grid">
-                <SortableList axis="xy" items={this.state.items} onSortEnd={this.onSortEnd}/>
+                <SortableList axis="xy"
+                    items={this.state.items}
+                    onSortEnd={this.onSortEnd}
+                    pressDelay={250}
+                />
             </div>
         );
     }
