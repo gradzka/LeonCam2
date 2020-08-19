ae6950d5d86d43d26db3e544fc39e296115779c3
gradzka <monika.gradzka@tlen.pl>
2020-08-19 21:36:49 +0200

    Code refactor

--- LeonCam2/ClientApp/src/components/Dashboard/Dashboard.js
+++ LeonCam2/ClientApp/src/components/Dashboard/Dashboard.js
@@ -1,60 +1,10 @@
 ﻿import React, { Component } from 'react';
 import { SortableContainer, SortableElement } from "react-sortable-hoc";
 import arrayMove from "array-move";
-import { Container, Row, Col } from 'reactstrap';
 import "./Dashboard.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPowerOff, faCamera, faEdit, faTrash, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
 import { Camera } from "../Shared/Camera.js";
-import { browserHistory } from '../../router/BrowserHistory';
+import {SortableCamera} from "./SortableCamera.js"
 
-const CircleActionButton = ({ icon, className, onClickAction }) => (
-    <Col xs="auto" className="padding-right-left-5" >
-        <button className={"circle " + className} onClick={onClickAction}>
-            <FontAwesomeIcon icon={icon} />
-        </button>
-    </Col>
-);
-
-class SortableCamera extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOn: false
-        }
-
-        this.powerOnOff = this.powerOnOff.bind(this);
-    }
-
-    powerOnOff(event) {
-        this.setState({
-            isOn: !this.state.isOn
-        });
-        this.props.onPowerOff(this.props.indexCopy);
-    }
-
-    render() {
-        return (
-            <div className="dashboard-grid-cell">
-                <Container>
-                    <Row className="justify-content-center padding-top-bottom-5 no-select">
-                        {this.props.camera.name}
-                    </Row>
-                    <Row>
-                        <div className="camera-container"></div>
-                    </Row>
-                    <Row className="justify-content-center padding-top-bottom-5">
-                        <CircleActionButton icon={faPowerOff} onClickAction={this.powerOnOff} className={this.state.isOn ? 'leon-green' : 'leon-red'} />
-                        <CircleActionButton icon={faExpandAlt} onClickAction={() => browserHistory.push('/camerafullscreen', { id: this.props.camera.id })} />
-                        <CircleActionButton icon={faEdit} onClickAction={() => browserHistory.push('/cameraedition', { id: this.props.camera.id }) } />
-                        <CircleActionButton icon={faCamera} onClickAction={() => this.props.onTakePhoto(this.props.camera)} />
-                        <CircleActionButton icon={faTrash} onClickAction={() => this.props.onRemove(this.props.indexCopy, this.props.camera)} className="leon-red" />
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
-}
 
 const SortableItem = SortableElement(({ indexCopy, camera, onPowerOff, onRemove, onTakePhoto }) => {
     return (
@@ -72,22 +22,40 @@ const SortableList = SortableContainer(({ items, onPowerOff, onRemove, onTakePho
     );
 });
 
-class SortableComponent extends Component {
+export class Dashboard extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            items: Array.apply(null, this.props.items).map((camera, index) => camera)
+            items: []
         };
-    }   
 
-    componentDidMount() { }
+        this.getCameras = this.getCameras.bind(this);
+    } 
+
+    componentDidMount() {
+        this.getCameras();
+    }
 
     powerOff = (camera) => {
 
     }
 
-    remove = ( index, camera ) => {
+    getCameras() {
+        let cameras = [
+            new Camera(1, "Baby room", "192.168.1.1"),
+            new Camera(2, "Living room", "192.168.1.2"),
+            new Camera(3, "Front door", "192.168.1.3"),
+            new Camera(4, "Garage", "192.168.1.4"),
+            new Camera(5, "Garden", "192.168.1.5")
+        ]
+
+        this.setState({
+            items: Array.apply(null, cameras).map((camera, index) => camera)
+        });
+    }
+
+    remove = (index, camera) => {
         this.state.items.splice(index, 1);
         this.setState({ items: this.state.items });
     }
@@ -105,25 +73,8 @@ class SortableComponent extends Component {
     render() {
         return (
             <div className="dashboard-grid">
-                <SortableList axis="xy" items={this.state.items} onPowerOff={this.powerOff} onRemove={this.remove} onSortEnd={this.sortEnd} onTakePhoto={this.takePhoto} pressDelay={200}/>
+                <SortableList axis="xy" items={this.state.items} onPowerOff={this.powerOff} onRemove={this.remove} onSortEnd={this.sortEnd} onTakePhoto={this.takePhoto} pressDelay={200} />
             </div>
-        );
-    }
-}
-
-export class Dashboard extends Component {
-    render() {
-        return (
-            <SortableComponent items=
-                {
-                    [
-                        new Camera(1, "Baby room", "192.168.1.1"),
-                        new Camera(2, "Living room", "192.168.1.2"),
-                        new Camera(3, "Front door", "192.168.1.3"),
-                        new Camera(4, "Garage", "192.168.1.4"),
-                        new Camera(5, "Garden", "192.168.1.5")
-                    ]
-                } />
         );
     }
 }
---
+++ LeonCam2/ClientApp/src/components/Dashboard/SortableCamera.js
@@ -0,0 +1,54 @@
+﻿import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPowerOff, faCamera, faEdit, faTrash, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+import { browserHistory } from '../../router/BrowserHistory';
+
+const CircleActionButton = ({ icon, className, onClickAction }) => (
+    <Col xs="auto" className="padding-right-left-5" >
+        <button className={"circle " + className} onClick={onClickAction}>
+            <FontAwesomeIcon icon={icon} />
+        </button>
+    </Col>
+);
+
+export class SortableCamera extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOn: false
+        }
+
+        this.powerOnOff = this.powerOnOff.bind(this);
+    }
+
+    powerOnOff(event) {
+        this.setState({
+            isOn: !this.state.isOn
+        });
+
+        this.props.onPowerOff(this.props.indexCopy);
+    }
+
+    render() {
+        return (
+            <div className="dashboard-grid-cell">
+                <Container>
+                    <Row className="justify-content-center padding-top-bottom-5 no-select">
+                        {this.props.camera.name}
+                    </Row>
+                    <Row>
+                        <div className="camera-container"></div>
+                    </Row>
+                    <Row className="justify-content-center padding-top-bottom-5">
+                        <CircleActionButton icon={faPowerOff} onClickAction={this.powerOnOff} className={this.state.isOn ? 'leon-green' : 'leon-red'} />
+                        <CircleActionButton icon={faExpandAlt} onClickAction={() => browserHistory.push('/camerafullscreen', { id: this.props.camera.id })} />
+                        <CircleActionButton icon={faEdit} onClickAction={() => browserHistory.push('/cameraedition', { id: this.props.camera.id })} />
+                        <CircleActionButton icon={faCamera} onClickAction={() => this.props.onTakePhoto(this.props.camera)} />
+                        <CircleActionButton icon={faTrash} onClickAction={() => this.props.onRemove(this.props.indexCopy, this.props.camera)} className="leon-red" />
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
