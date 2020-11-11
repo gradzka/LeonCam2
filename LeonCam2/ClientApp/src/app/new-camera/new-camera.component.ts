a5ef5d0527cca3f4b9c668bc94a1b8ad9b8bcc55
gradzka <monika.gradzka@tlen.pl>
2020-11-11 21:06:35 +0100

    Fix new camera front, add submit button

--- LeonCam2/ClientApp/src/app/new-camera/new-camera.component.ts
+++ LeonCam2/ClientApp/src/app/new-camera/new-camera.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,11 +10,14 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./new-camera.component.css']
 })
 
-export class NewCameraComponent implements OnInit { 
+export class NewCameraComponent implements OnInit {
+  addCameraLoading = false;
   filteredCameras: string[];
   icon = faSearch;
   newCameraForm: FormGroup;
   searchedCameras: string[];
+
+  @ViewChild("addCameraPopover") public addCameraPopover: NgbPopover;
 
   constructor(
     private formBuilder: FormBuilder
@@ -20,7 +25,10 @@ export class NewCameraComponent implements OnInit {
 
   ngOnInit() {
     this.newCameraForm = this.formBuilder.group({
-      ip: ['', Validators.required]
+      description: ['', Validators.required],
+      ip: ['', Validators.required],
+      login: ['', Validators.required],
+      password: ['', Validators.required],
     });
 
     this.newCameraControls.ip.valueChanges.subscribe(value => {
@@ -31,6 +39,38 @@ export class NewCameraComponent implements OnInit {
   }
 
   get newCameraControls() { return this.newCameraForm.controls; }
+
+  addCamera(event) {
+    this.addCameraPopover.close();
+
+    if (this.newCameraForm.invalid) {
+      this.addCameraPopover.ngbPopover = "Type valid data";
+      this.addCameraPopover.popoverClass = "popover-error-reversed";
+      this.addCameraPopover.open();
+      return;
+    }
+
+    //this.addCameraLoading = true;
+    //this.userService.changeUsername(this.newCameraControls.changeUsernameNewUsername.value, this.newCameraControls.changeUsernamePassword.value)
+    //  .pipe(first())
+    //  .subscribe({
+    //    next: () => {
+    //      this.newCameraControls.changeUsernamePassword.setValue("");
+    //      this.addCameraPopover.ngbPopover = "Success";
+    //      this.addCameraPopover.popoverClass = "popover-success-reversed";
+    //      this.addCameraPopover.open();
+    //      this.addCameraLoading = false;
+    //    },
+    //    error: error => {
+    //      this.addCameraLoading = false;
+    //      this.addCameraPopover.popoverClass = "popover-error-reversed";
+    //      this.addCameraPopover.ngbPopover = error === "Unexpected error" ? "Add New Camera Error" : error;
+    //      this.addCameraPopover.open();
+    //    }
+    //  });
+
+    event.preventDefault();
+  }
 
   filterCameras(event) {
     this.filteredCameras = this.searchedCameras.filter(x => x.toLowerCase().startsWith(event.toLowerCase()));
