c15d78c35056f07df6b6b0aa24e6af9a8c271207
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2020-12-13 19:49:55 +0100

    CryptoService

--- LeonCam2/Extensions/StringExtensions.cs
+++ LeonCam2/Extensions/StringExtensions.cs
@@ -2,18 +2,8 @@
 
 namespace LeonCam2.Extensions
 {
-    using System;
-    using System.Security.Cryptography;
-    using System.Text;
-
     public static class StringExtensions
     {
-        public static string GetSHA512Hash(this string parameter)
-        {
-            using SHA512 sHA512 = new SHA512Managed();
-            return BitConverter.ToString(sHA512.ComputeHash(Encoding.Default.GetBytes(parameter))).Replace("-", string.Empty);
-        }
-
         public static bool IsNullOrEmpty(this string parameter)
         {
             return string.IsNullOrEmpty(parameter);
---
+++ LeonCam2/Services/Security/CryptoService.cs
@@ -0,0 +1,22 @@
+﻿// CryptoService.cs by Gradzka & Kazimierczak
+
+namespace LeonCam2.Services.Security
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class CryptoService : ICryptoService
+    {
+        public string GetSHA512Hash(string input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+
+            using SHA512 sHA512 = new SHA512Managed();
+            return BitConverter.ToString(sHA512.ComputeHash(Encoding.Default.GetBytes(input))).Replace("-", string.Empty);
+        }
+    }
+}
