12435a491d50886059dbafee0fb4b97d014fed2e
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2021-09-19 16:36:42 +0200

    Remove example code

--- LeonCam2/ClientApp/src/app/app.component.ts
+++ LeonCam2/ClientApp/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastService } from './toasts/toast.service';
 
 import { AuthenticationService } from './_services/authentication.service';
 
@@ -14,35 +13,12 @@ export class AppComponent {
 
   constructor(
     private router: Router,
-    private authenticationService: AuthenticationService,
-    private toastService: ToastService
-  ) {
+    private authenticationService: AuthenticationService) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
 
   logout() {
     this.authenticationService.logout();
     this.router.navigate(['']);
-  }
-
-
-  showStandard() {
-    this.toastService.showStandard('I am a standard toast!!!!!!!!!!!!!!!!');
-  }
-
-  showSuccess() {
-    this.toastService.showSuccess('I am a success toast');
-  }
-
-  showDanger() {
-    this.toastService.showError("Error toasr");
-  }
-
-  showWarning() {
-    this.toastService.showWarning("Warning");
-  }
-
-  showInfo() {
-    this.toastService.showInfo("Info");
   }
 }
